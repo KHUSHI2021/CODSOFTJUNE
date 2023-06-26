2f6643f28dc755f2d3a42de5d8ebe1f214fa50ad
khushi <110249789+KHUSHI2021@users.noreply.github.com>
2023-06-26 19:55:15 +0530

    Add files via upload
    Netflix landing page specific for animes by using JAVASCRIPT,HTML,CSS
    level-1
    task-1

---
+++ index.js
@@ -0,0 +1,38 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e) {
+	// Remove all show and border classes
+	removeBorder();
+	removeShow();
+	// Add border to current tab item
+	this.classList.add('tab-border');
+	// Grab content item from DOM
+	const tabContentItem = document.querySelector(`#${this.id}-content`);
+	// Add show class
+	tabContentItem.classList.add('show');
+}
+
+// Remove bottom borders from all tab items
+function removeBorder() {
+	tabItems.forEach(item => {
+		item.classList.remove('tab-border');
+	});
+}
+
+// Remove show class from all content items
+function removeShow() {
+	tabContentItems.forEach(item => {
+		item.classList.remove('show');
+	});
+}
+
+// Listen for tab item click
+tabItems.forEach(item => {
+	item.addEventListener('click', selectItem);
+});
+//https://cellularnews.com/wp-content/uploads/2020/05/Funimation-anime-600x376.jpeg
+
+
+//https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzk5I8_XNSMCEXAliq3HYvAI6g8v4ZUKzvwg&usqp=CAU
